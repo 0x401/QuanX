fdb0d3892087d4e204874f241330207468631ae8
0x401 <0x401@users.noreply.github.com>
2024-02-01 10:21:03 +0800

    热门微博标题

--- weibo.js
+++ weibo.js
@@ -617,8 +617,8 @@ if (url.includes("/interface/sdk/sdkad.php")) {
          //  }
             newItems.push(item);
           } else if (item?.category === "card") {
-            // 19热议等tab 118横版广告图片 206,249横版视频广告 208实况热聊 217错过了热词
-            if ([19, 118, 206, 208, 217, 249]?.includes(item?.data?.card_type)) {
+            // 19热议等tab 118横版广告图片 206,249横版视频广告 208实况热聊 217错过了热词,101热门微博标题
+            if ([19, 101, 118, 206, 208, 217, 249]?.includes(item?.data?.card_type)) {
             continue;           
             }
             newItems.push(item);
