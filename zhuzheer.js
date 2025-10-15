26a9aa72d7346245491ee8d35e2df360d0fd71b1
0x401 <0x401@users.noreply.github.com>
2025-10-15 14:00:09 +0800

    Update zhuzheer.js

--- zhuzheer.js
+++ zhuzheer.js
@@ -2,8 +2,8 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/proteus/api/config/global/app")) { 
-  //删除机器人提示
+if (url.includes("/proteus/api/config/global/app") || url.includes("/proteus/api/config/global/h5")) { 
+  //删除机器人
   if(obj?.result?.aiBot){
     delete obj.result.aiBot;
   }
@@ -12,6 +12,11 @@ else if (url.includes("/ads/v3/type/") || url.includes("/genesis/openApi/post/re
   //删除首页广告社区用户贴
   if(obj?.result){
     obj.result = []
+  }
+}
+else if (url.includes("/jtouch/api/msg/v2/home")){
+  if(obj?.result?.latest){
+    obj.result.latest = obj.result.latest.filter((item) => item.groupType = 1);
   }
 }
 console.log(obj)
