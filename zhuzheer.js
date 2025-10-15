d8015a7c2259d1d10c4f5750003cf47f7333e189
0x401 <0x401@users.noreply.github.com>
2025-10-15 12:56:25 +0800

    更新 zhuzheer.js

--- zhuzheer.js
+++ zhuzheer.js
@@ -3,15 +3,16 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/proteus/api/config/global/app")) { 
-  #删除机器人提示
+  //删除机器人提示
   if(obj?.result?.aiBot){
     delete obj.result.aiBot;
   }
 }
 else if (url.includes("/ads/v3/type/") || url.includes("/genesis/openApi/post/recommend/help")) { 
-  #删除首页广告社区用户贴
+  //删除首页广告社区用户贴
   if(obj?.result){
     obj.result = []
   }
 }
+console.log(obj)
 $done({ body: JSON.stringify(obj) });
