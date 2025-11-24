a3153966146e87462637424c8e1731ddf7d1e90b
0x401 <0x401@users.noreply.github.com>
2025-11-24 13:25:26 +0800

    delete ad

--- ithome.js
+++ ithome.js
@@ -4,11 +4,15 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("/api/news/indexv2/iphone/")) {
     if (obj?.data?.list) {
+        let newList = []
         obj.data.list.forEach(l => {
+            if(l?.feedContent?.flag != 2){
             if (l?.feedContent?.content) {
                 l.feedContent.content = l.feedContent.content.replace(/(<div class='tougao-user' style='display:none;'>.*?<\/div>|IT之家[^，。：]*[，。：]|<p class=\\"ad-tips\\">.*?<\/p>)/g, "")
             }
+            newList.push(l)}
         })
+        obj.data.list = newList
     }
 }
 
