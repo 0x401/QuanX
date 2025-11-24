0131161214abc70a46837bc23f864d69053cd718
0x401 <0x401@users.noreply.github.com>
2025-11-24 11:04:07 +0800

    更新 ithome.js

--- ithome.js
+++ ithome.js
@@ -2,16 +2,16 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/api/news/indexv2/iphone/")) { 
-  //删除机器人
+if (url.includes("/api/news/indexv2/iphone/")) {
+    if (obj?.data?.list) {
+        obj.data.list.forEach(l => {
+            if (l?.feedContent?.content) {
+                l.feedContent.content.replace("IT之家", "")
+            }
+        })
+    }
+}
 
-if(obj?.data?.list){
-    obj.data.list.forEach(l=>{
-        if(l?.feedContent?.content){
-            l.feedContent.content.replace("IT之家","")
-        }
-    })
-}  
-
-
-$done({ body: JSON.stringify(obj) });
+$done({
+    body: JSON.stringify(obj)
+});
