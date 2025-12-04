8309c1c85f2dab22c164bc06efecb6ab9a04216d
0x401 <0x401@users.noreply.github.com>
2025-12-04 09:48:05 +0800

    Refactor news list filtering and ad removal logic

--- ithome.js
+++ ithome.js
@@ -3,19 +3,28 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/api/news/indexv2/iphone/")) {
-    if (obj?.data?.list) {
-        let newList = []
-        obj.data.list.forEach(l => {
-            if(l?.feedContent?.flag != 2){
-            if (l?.feedContent?.content) {
-                l.feedContent.content = l.feedContent.content.replace(/(<div class='tougao-user'[^>]*>([\s\S]*)<\/div>|<p class="ad-tips"[^>]*>([\s\S]*)<\/p>|IT\s?之家[^，。：]*[，。：])/g, "")
-            }
-            newList.push(l)}
-        })
-        obj.data.list = newList
-    }
+  if (obj?.data?.list) {
+    let newList = [];
+    obj.data.list.forEach((l) => {
+      if (l?.feedContent?.focusNewsData) {
+        l.feedContent.focusNewsData = l.feedContent.focusNewsData.filter(
+          (d) => !d.isAd,
+        );
+      }
+      if (l?.feedContent?.flag != 2) {
+        if (l?.feedContent?.content) {
+          l.feedContent.content = l.feedContent.content.replace(
+            /(<div class='tougao-user'[^>]*>([\s\S]*)<\/div>|<p class="ad-tips"[^>]*>([\s\S]*)<\/p>|IT\s?之家[^，。：]*[，。：])/g,
+            "",
+          );
+        }
+        newList.push(l);
+      }
+    });
+    obj.data.list = newList;
+  }
 }
 
 $done({
-    body: JSON.stringify(obj)
+  body: JSON.stringify(obj),
 });
