4b770996f8de27874dcc6cb2116c27abc74d5601
0x401 <0x401@users.noreply.github.com>
2026-02-04 10:31:16 +0800

    Enhance news filtering and content cleanup
    Filter out articles by feed type and remove unnecessary information from news content.

--- ithome.js
+++ ithome.js
@@ -6,11 +6,15 @@ if (url.includes("/api/news/indexv2/iphone/")) {
   if (obj?.data?.list) {
     let newList = [];
     obj.data.list.forEach((l) => {
+      //按文章类型去除（10003 置顶，10004 推广红包倒计时？）
+      if(l.?feedType == 10003 || l.?feedType == 10004) continue;
+      //去掉轮播里有ad tag的
       if (l?.feedContent?.focusNewsData) {
         l.feedContent.focusNewsData = l.feedContent.focusNewsData.filter(
           (d) => !d.isAd,
         );
       }
+      //去掉新闻内容里无用的信息
       if (l?.feedContent?.flag != 2) {
         if (l?.feedContent?.content) {
           l.feedContent.content = l.feedContent.content.replace(
